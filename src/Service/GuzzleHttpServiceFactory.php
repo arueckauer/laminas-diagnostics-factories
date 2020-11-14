af61b9f193407c2a6854b189cd70ef64ebeddb78
Andi Rückauer <arueckauer@gmail.com>
2020-11-14 11:05:48 +0100

    Add factory for GuzzleHttpService

---
+++ src/Service/GuzzleHttpServiceFactory.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace arueckauer\LaminasDiagnosticsFactories\Service;
+
+use GuzzleHttp\ClientInterface as GuzzleClientInterface;
+use Laminas\Diagnostics\Check\GuzzleHttpService;
+use Psr\Container\ContainerInterface;
+
+class GuzzleHttpServiceFactory
+{
+    public function __invoke(ContainerInterface $container): GuzzleHttpService
+    {
+        $params = $container->get('config')['system']['health-check']['params'][GuzzleHttpService::class] ?? [];
+        return new GuzzleHttpService(
+            $this->getRequestOrUrl($params),
+            $this->getHeaders($params),
+            $this->getOptions($params),
+            $this->getStatusCode($params),
+            $this->getContent($params),
+            $this->getGuzzle($params),
+            $this->getMethod($params),
+            $this->getBody($params)
+        );
+    }
+
+    private function getRequestOrUrl(array $params): string
+    {
+        return $params['requestOrUrl'];
+    }
+
+    private function getHeaders(array $params): array
+    {
+        return $params['headers'] ?? [];
+    }
+
+    private function getOptions(array $params): array
+    {
+        return $params['options'] ?? [];
+    }
+
+    private function getStatusCode(array $params): int
+    {
+        return $params['statusCode'] ?? 200;
+    }
+
+    private function getContent(array $params): ?string
+    {
+        return $params['content'] ?? null;
+    }
+
+    private function getGuzzle(array $params): ?GuzzleClientInterface
+    {
+        return $params['guzzle'] ?? null;
+    }
+
+    private function getMethod(array $params): string
+    {
+        return $params['method'] ?? 'GET';
+    }
+
+    /**
+     * @return mixed|null
+     */
+    private function getBody(array $params)
+    {
+        return $params['body'] ?? null;
+    }
+}
