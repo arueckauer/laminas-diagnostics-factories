dea70f6d13e5d48c4b43f869acbd579bc9ae3abe
Andi Rückauer <arueckauer@gmail.com>
2020-11-14 11:06:09 +0100

    Add factory for PhpVersion

---
+++ src/Service/PhpVersionFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace arueckauer\LaminasDiagnosticsFactories\Service;
+
+use Laminas\Diagnostics\Check\PhpVersion;
+use Psr\Container\ContainerInterface;
+
+class PhpVersionFactory
+{
+    public function __invoke(ContainerInterface $container): PhpVersion
+    {
+        $params = $container->get('config')['system']['health-check']['params'][PhpVersion::class] ?? [];
+        return new PhpVersion(
+            $this->getExpectedVersion($params),
+            $this->getOperator($params)
+        );
+    }
+
+    private function getExpectedVersion(array $params): string
+    {
+        return $params['expectedVersion'];
+    }
+
+    private function getOperator(array $params): string
+    {
+        return $params['operator'] ?? '>=';
+    }
+}
