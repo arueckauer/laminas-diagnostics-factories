fba5db69be02627b905611161f71b33ba81d96c6
Andi Rückauer <arueckauer@gmail.com>
2020-11-14 11:06:20 +0100

    Add factory for SecurityAdvisory

---
+++ src/Service/SecurityAdvisoryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace arueckauer\LaminasDiagnosticsFactories\Service;
+
+use Laminas\Diagnostics\Check\SecurityAdvisory;
+use Psr\Container\ContainerInterface;
+
+class SecurityAdvisoryFactory
+{
+    public function __invoke(ContainerInterface $container): SecurityAdvisory
+    {
+        $params = $container->get('config')['system']['health-check']['params'][SecurityAdvisory::class] ?? [];
+        return new SecurityAdvisory(
+            $this->getLockFilePath($params)
+        );
+    }
+
+    private function getLockFilePath(array $params): ?string
+    {
+        return $params['lockFilePath'] ?? null;
+    }
+}
