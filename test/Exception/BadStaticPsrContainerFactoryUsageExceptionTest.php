b54cce6d22b3a77d20c2514cde303abeba4d4d93
Andi Rückauer <arueckauer@gmail.com>
2021-08-20 12:50:23 +0200

    Add BadStaticPsrContainerFactoryUsageExceptionTest

---
+++ test/Exception/BadStaticPsrContainerFactoryUsageExceptionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Exception;
+
+use LaminasDiagnosticsFactories\Exception\BadStaticPsrContainerFactoryUsageException;
+use PHPUnit\Framework\TestCase;
+
+class BadStaticPsrContainerFactoryUsageExceptionTest extends TestCase
+{
+    /**
+     * @covers \LaminasDiagnosticsFactories\Exception\BadStaticPsrContainerFactoryUsageException::missingContainerArgument
+     */
+    public function test_missingContainerArgument(): void
+    {
+        self::assertSame(
+            'The first argument for Foo must be an instance of Psr\\Container\\ContainerInterface.',
+            BadStaticPsrContainerFactoryUsageException::missingContainerArgument('Foo')->getMessage()
+        );
+    }
+}
