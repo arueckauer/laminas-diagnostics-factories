0f9986cb4f456b58d7cffcf0c4921b7302ae3453
Andi Rückauer <arueckauer@gmail.com>
2024-11-25 12:08:20 +0100

    Removes redundant Exception suffix from BadStaticPsrContainerFactoryUsageException

--- test/LaminasDiagnosticsFactories/Exception/BadStaticPsrContainerFactoryUsageTest.php
+++ test/LaminasDiagnosticsFactories/Exception/BadStaticPsrContainerFactoryUsageTest.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace LaminasDiagnosticsFactories\Exception;
 
-use LaminasDiagnosticsFactories\Exception\BadStaticPsrContainerFactoryUsageException;
+use LaminasDiagnosticsFactories\Exception\BadStaticPsrContainerFactoryUsage;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
-#[CoversClass(BadStaticPsrContainerFactoryUsageException::class)]
-class BadStaticPsrContainerFactoryUsageExceptionTest extends TestCase
+#[CoversClass(BadStaticPsrContainerFactoryUsage::class)]
+class BadStaticPsrContainerFactoryUsageTest extends TestCase
 {
     public function test_missingContainerArgument(): void
     {
         self::assertSame(
             'The first argument for Foo must be an instance of Psr\\Container\\ContainerInterface.',
-            BadStaticPsrContainerFactoryUsageException::missingContainerArgument('Foo')->getMessage()
+            BadStaticPsrContainerFactoryUsage::missingContainerArgument('Foo')->getMessage()
         );
     }
 }
