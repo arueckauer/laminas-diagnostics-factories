a0f63935bb9895a51d7dedc03b33fe16d6f94669
Andi Rückauer <arueckauer@gmail.com>
2024-11-25 11:08:06 +0100

    Adds test for PDOCheckPsrContainerFactory

---
+++ test/LaminasDiagnosticsFactories/PDOCheckPsrContainerFactoryTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasDiagnosticsFactoriesTest;
+
+use LaminasDiagnosticsFactories\PDOCheckPsrContainerFactory;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+#[CoversClass(PDOCheckPsrContainerFactory::class)]
+class PDOCheckPsrContainerFactoryTest extends TestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function test__invoke()
+    {
+        $factory = new PDOCheckPsrContainerFactory('pdo_check');
+
+        $container = $this->createMock(ContainerInterface::class);
+        $container
+            ->expects(self::once())
+            ->method('has')
+            ->with('config')
+            ->willReturn(true);
+        $container
+            ->expects(self::once())
+            ->method('get')
+            ->with('config')
+            ->willReturn([
+                'diagnostics' => [
+                    'pdo_check' => [
+                        'dsn'      => 'mysql:host=localhost;dbname=test',
+                        'username' => 'db_user',
+                        'password' => 'db_password',
+                        'timeout'  => 1,
+                    ],
+                ],
+            ]);
+
+        $check = $factory($container);
+
+        self::assertSame('Check if mysql:host=localhost;dbname=test can be reached', $check->getLabel());
+    }
+}
